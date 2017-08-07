ac8308cdbcd4fa2c0306e6e7b4fe938d63d079d9
Fred Chien <cfsghost@gmail.com>
2017-08-08 01:51:34 +0800

    implement paginations

--- apis/admin/Member.js
+++ apis/admin/Member.js
@@ -15,6 +15,47 @@ module.exports = (service) => {
 	router.use(service.routeType('API'));
 
 	/**
+	 * @api {get} /api/v1/admin/member List members
+	 * @apiName ListMembers
+	 * @apiGroup Admin
+	 *
+	 * @apiHeader {String} authorization admin's token
+	 * @apiParam {Number} perPage Number of results per page
+	 * @apiParam {Number} page Page number
+	 * @apiParam {Object} conditions Conditions for querying
+	 *
+	 * @apiSuccess {Array} Member List
+	 *
+	 * @apiError 404 NotFound Account doesn't exist
+	 **/
+	router.get('/admin/member', Permission('Admin.access'), Permission('Member.list'), async (ctx, next) => {
+
+		const payload = ctx.request.query;
+
+		try {
+			// Getting member profile
+			const members = await memberAgent
+				.getMemberManager()
+				.listMembers(payload.conditions, parseInt(payload.page), parseInt(payload.perPage));
+
+			// Response
+			ctx.body = members;
+
+		} catch(e) {
+
+			switch(e.name) {
+			case 'NotExist':
+				ctx.throw(404, {
+					code: 'NotExist',
+					message: 'Account doesn\'t exist'
+				});
+			}
+
+			throw e;
+		}
+	});
+
+	/**
 	 * @api {get} /api/v1/admin/member/:memberId Getting member profile
 	 * @apiName GetMemberProfile
 	 * @apiGroup Admin
